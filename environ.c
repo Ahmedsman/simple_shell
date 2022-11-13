b87a962a6e17fe9fe1c598d813aeb111d9b62dd7
Ahmedsman <ahmedsman11@gmail.com>
2022-11-13 11:25:27 -0800

    Simple Shell

--- environ.c
+++ environ.c
@@ -2,9 +2,9 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain.
  *          constant function prototype.
- * Return: Always 0
+ * Return: Always 0.
  */
 int _myenv(info_t *info)
 {
--- parser.c
+++ parser.c
@@ -2,8 +2,8 @@
 
 /**
  * is_cmd - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
+ * @info: the info struct.
+ * @path: path to the file.
  *
  * Return: 1 if true, 0 otherwise
  */
